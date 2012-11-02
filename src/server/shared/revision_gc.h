9fb424a43f69501792e0c2f88408aec17c27f272
Vitasic <get_email Vitasic>
2012-11-02 19:11:57 +0400

    Typo fix...
    Former-commit-id: 96fdfe82bf7adf2b22b453c374bacf941f100b9a

--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
-<<<<<<< HEAD
- #define REVISION_gc "479"
-=======
- #define REVISION_gc "466"
->>>>>>> parent of 2c140c1... [gc467] do not announce arena teams leaving queue
+#define REVISION_gc "479"
 #endif // __REVISION_gc_H__
