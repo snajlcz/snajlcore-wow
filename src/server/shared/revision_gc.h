449bf0d6ff9a39463e8fbf8384743b23d4a7ee56
Vitasic <get_email Vitasic>
2012-11-02 19:10:24 +0400

    revert Not announce arena

--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
+<<<<<<< HEAD
  #define REVISION_gc "479"
+=======
+ #define REVISION_gc "466"
+>>>>>>> parent of 2c140c1... [gc467] do not announce arena teams leaving queue
 #endif // __REVISION_gc_H__
