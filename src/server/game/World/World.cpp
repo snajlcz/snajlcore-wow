cda869ba2a295cff914584c0bfe34e63f8f3ed32
Vitasic <get_email Vitasic>
2012-11-18 17:37:43 +0400

    [Revert] Boss Kill History 1

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1256,9 +1256,6 @@ void World::LoadConfigSettings(bool reload)
     // Disable Chat
     m_int_configs[CONFIG_INT_CHAT_DISABLE_TIME]  = ConfigMgr::GetIntDefault("Chat.DisableWhenPlayerCreate", 60);
 
-    //Boss kill history system
-    m_bool_configs[CONFIG_BOSS_KILL_HISTORY_ENABLE] = ConfigMgr::GetBoolDefault("Boss.Kill.History.Enable", true);
-
     // call ScriptMgr if we're reloading the configuration
     m_bool_configs[CONFIG_WINTERGRASP_ENABLE] = ConfigMgr::GetBoolDefault("Wintergrasp.Enable", false);
     m_int_configs[CONFIG_WINTERGRASP_PLR_MAX] = ConfigMgr::GetIntDefault("Wintergrasp.PlayerMax", 100);
