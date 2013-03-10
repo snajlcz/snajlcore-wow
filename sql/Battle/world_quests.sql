8a18ca1ea73cd7d29a3cae0f62d1d454c4431219
Vitasic <get_email Vitasic>
2013-03-10 22:05:20 +0400

    fix quest http://ru.wowhead.com/quest=24556
    Former-commit-id: fbd9b8abc514b9273ce8edc8f73a6e8cf519ccfd

--- sql/Battle/world_quests.sql
+++ sql/Battle/world_quests.sql
@@ -19,3 +19,17 @@ UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=6001;
 UPDATE `quest_template` SET `RequiredNpcOrGo1`='12138' WHERE `Id`=6002; 
 UPDATE `quest_template` SET `RequiredNpcOrGoCount1`='1' WHERE `Id`=6002;
 UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=6002;
+
+-- Quests: A Suitable Disguise, http://old.wowhead.com/quest=20438
+--                              http://old.wowhead.com/quest=24556
+SET @ENTRY := 36856;
+SET @SOURCETYPE := 0;
+SET @GOSSIP := 10854;
+SET @MENUID := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,2,62,0,100,0,@GOSSIP,@MENUID,0,0,50,201384,60,0,0,0,0,8,0,0,0,5802.22,691.556,657.949,3.50801,"Arcanum Core - Script for quest http://wowhead.com/quest=24556"),
+(@ENTRY,@SOURCETYPE,1,2,62,0,100,0,@GOSSIP,@MENUID+1,0,0,50,201384,60,0,0,0,0,8,0,0,0,5802.22,691.556,657.949,3.50801,"Arcanum Core - Script for quest http://wowhead.com/quest=20438"),
+(@ENTRY,@SOURCETYPE,2,0,61,0,100,0,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,"On gossip option select - Close gossip");
