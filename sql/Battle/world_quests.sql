bcd4fbba257c663de929f5d18afe3cf168c3e2cc
Vitasic <get_email Vitasic>
2013-03-10 18:01:54 +0400

    [SQL] Fix druid bear quests Horde/Alliance
    Former-commit-id: 0dc15647356bf95102270c25f19b3d612657417f

---
+++ sql/Battle/world_quests.sql
@@ -0,0 +1,21 @@
+﻿set names utf8;
+
+-- nagrand_arena_last_boss_quest
+--UPDATE `quest_template` SET `RequiredNpcOrGo1` = '18069',`RequiredNpcOrGoCount1` = '1' WHERE `Id` =9977;
+--UPDATE `quest_template` SET `SpecialFlags` = '0' WHERE `Id` =9977;
+--UPDATE `quest_template` SET `RequiredNpcOrGo1`='18069' WHERE `Id`=9977; 
+--UPDATE `quest_template` SET `RequiredNpcOrGoCount1`='1' WHERE `Id`=9977;
+--UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=9977;
+---UPDATE `quest_template` SET `RequiredNpcOrGo1`='30022' WHERE `Id`=12948; 
+---UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=12948;
+
+-- fix  quests 4921 "Пропавшая без вести"
+UPDATE `creature_template` SET `npcflag` = 3 WHERE `entry` = 10668;
+
+-- fix quests 6001,6002 Druid
+UPDATE `quest_template` SET `RequiredNpcOrGo1`='12138' WHERE `Id`=6001; 
+UPDATE `quest_template` SET `RequiredNpcOrGoCount1`='1' WHERE `Id`=6001;
+UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=6001;
+UPDATE `quest_template` SET `RequiredNpcOrGo1`='12138' WHERE `Id`=6002; 
+UPDATE `quest_template` SET `RequiredNpcOrGoCount1`='1' WHERE `Id`=6002;
+UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=6002;
