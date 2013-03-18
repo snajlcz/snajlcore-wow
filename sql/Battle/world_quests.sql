52d6100968532c3ba41eb3c69078b193382cc51e
Vitasic <get_email Vitasic>
2013-03-18 19:51:18 +0400

    Добавление лута при рыбалке в Даларане.
    Former-commit-id: 2c6de5f038e185a093ef6c1ba9e6f695d2925414 [formerly 844aeca65731a4e8fe58914c4054e38524a340c6]
    Former-commit-id: 5055a15bb1c0480a78a840cbb4085046b42822e4

--- sql/Battle/world_quests.sql
+++ sql/Battle/world_quests.sql
@@ -105,3 +105,8 @@ UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,1,62,0,100,0,8161,0,0,0,33,20678,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"Arcanum Core - Script quest 10368"),
 (@ENTRY,@SOURCETYPE,1,0,61,0,100,0,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"Arcanum Core - Script quest 10368");
+
+-- Dalaran
+-- Missing fishing loot
+INSERT INTO `fishing_loot_template`(`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES 
+( '4395','43630','0.2','1','1','1','1');
