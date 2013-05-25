e4360d3e91491a5660e6860f1a47be1ccb706c48
Vitasic <get_email Vitasic>
2013-05-25 19:00:59 +0400

    DB/Quest: Fix: Standards and Practices, Standarten
    Former-commit-id: 6ef6a707e80597aee86dd4e5d223c52b8b45b15c

--- sql/Battle/world_quests.sql
+++ sql/Battle/world_quests.sql
@@ -1,14 +1,4 @@
 ﻿set names utf8;
-
--- nagrand_arena_last_boss_quest
---UPDATE `quest_template` SET `RequiredNpcOrGo1` = '18069',`RequiredNpcOrGoCount1` = '1' WHERE `Id` =9977;
---UPDATE `quest_template` SET `SpecialFlags` = '0' WHERE `Id` =9977;
---UPDATE `quest_template` SET `RequiredNpcOrGo1`='18069' WHERE `Id`=9977; 
---UPDATE `quest_template` SET `RequiredNpcOrGoCount1`='1' WHERE `Id`=9977;
---UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=9977;
----UPDATE `quest_template` SET `RequiredNpcOrGo1`='30022' WHERE `Id`=12948; 
----UPDATE `quest_template` SET `SpecialFlags`=0 WHERE `Id`=12948;
-
 -- fix  quests 4921 "Пропавшая без вести"
 UPDATE `creature_template` SET `npcflag` = 3 WHERE `entry` = 10668;
 
@@ -81,7 +71,6 @@ UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry`=29775;
 -- Quest 10368
 SET @ENTRY := 20678;
 SET @SOURCETYPE := 0;
-
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
@@ -90,7 +79,6 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 
 SET @ENTRY := 20677;
 SET @SOURCETYPE := 0;
-
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
@@ -99,7 +87,6 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 
 SET @ENTRY := 20679;
 SET @SOURCETYPE := 0;
-
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
@@ -129,3 +116,33 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (18069,0,5,0,6,0,100,0,0,0,0,0,15,9977,0,0,0,0,0,16,0,0,0,0,0,0,0,"Mogor - On Death - Give Quest Credit");
 UPDATE `creature_template` SET `flags_extra`=`flags_extra`|0x10000000 WHERE `entry`=18069;
 UPDATE `creature` SET `spawntimesecs`=0 WHERE `id`=18069;
+
+-- DB/Quest: Fix: Standards and Practices
+-- DB/Quest: Fix: Standarten
+SET @SpellBattleStandard := 32205; -- Place Mag'har Battle Standard
+SET @FirstTriggerNPC := 18305; -- First trigger NPC
+SET @SecondTriggerNPC := 18306; -- Second trigger NPC
+SET @ThirdTriggerNPC := 18307; -- Third trigger NPC
+SET @FirstBB := 182261; -- First Burning Blade Pyre
+SET @SecondBB := 182264; -- Second Burning Blade Pyre
+SET @ThirdBB := 182262; -- Third Burning Blade Pyre
+SET @TriggerBB := 182263; -- First Burning Blade Pyre (Trigger GO)
+-- Spawnpoints of Burning Blades
+DELETE FROM `gameobject` WHERE `id` IN (@FirstBB, @SecondBB, @ThirdBB, @TriggerBB);
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
+(22718, @TriggerBB, 530, 1, 1, -2532.98, 6307.27, 14.0203, 2.95833, 0, 0, 0.995805, 0.091502, 181, 100, 1),
+(379, @TriggerBB, 530, 1, 1, -2533.19, 6168.61, 59.9391, 2.81871, 0, 0, 0.986996, 0.160743, 181, 100, 1),
+(371, @TriggerBB, 530, 1, 1, -2475.29, 6106.4, 91.9832, 1.98095, 0, 0, 0.836286, 0.548293, 181, 100, 1),
+(22716, @FirstBB, 530, 1, 1, -2532.98, 6307.27, 14.0203, 2.95833, 0, 0, 0.995805, 0.091502, 181, 100, 1),
+(22719, @SecondBB, 530, 1, 1, -2533.19, 6168.61, 59.9391, 2.81871, 0, 0, 0.986996, 0.160743, 181, 100, 1),
+(22717, @ThirdBB, 530, 1, 1, -2475.29, 6106.4, 91.9832, 1.98095, 0, 0, 0.836286, 0.548293, 181, 100, 1);
+-- Trigger NPC's for quest complete
+DELETE FROM `creature` WHERE `guid` IN (65640, 65641, 65642);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(65640, @FirstTriggerNPC, 530, 1, 1, 0, 0, -2532.98, 6307.27, 14.0203, 2.95833, 300, 0, 0, 800, 0, 0, 0, 0, 0),
+(65641, @SecondTriggerNPC, 530, 1, 1, 0, 0, -2533.19, 6168.61, 59.9391, 2.81871, 300, 0, 0, 800, 0, 0, 0, 0, 0),
+(65642, @ThirdTriggerNPC, 530, 1, 1, 0, 0, -2475.29, 6106.4, 91.9832, 1.98095, 300, 0, 0, 800, 0, 0, 0, 0, 0);
+-- Ignore LOS for trigger spell
+DELETE FROM `disables` WHERE `sourceType` = 0 AND `entry` = @SpellBattleStandard;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES
+(0, @SpellBattleStandard, 64, '', '', 'Ignore LOS for Place Mag''har Battle Standard');
