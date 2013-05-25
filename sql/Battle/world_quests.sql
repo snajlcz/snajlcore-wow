21d247e10a781b749ed3eaa55455cfdc0d94f46a
Vitasic <get_email Vitasic>
2013-05-25 19:51:25 +0400

    DB/Quest: Hack: Investigate the Blue Recluse.
    Former-commit-id: ce6e57d42847149bd6b6bff8089d0a8f0855ddc6

--- sql/Battle/world_quests.sql
+++ sql/Battle/world_quests.sql
@@ -391,3 +391,13 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,
 (13,1,69542,32,128,0,'Trigger Throw Pants requires target player'),
 (13,1,69544,32,128,0,'Trigger Throw Unmentionables requires target player'),
 (13,1,69543,32,128,0,'Trigger Throw Shirt requires target player');
+
+-- DB/Quest: Hack: Investigate the Blue Recluse
+-- DB/Quest: Hack: Untersuchungen im Blauen Eremiten
+SET @ENTRY = 6492;
+-- Add spawns for NPC Rift Spawn
+DELETE FROM `creature` WHERE id = @ENTRY;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
+(@ENTRY, 0, 1, 1, 0, 0, -9107.86, 832.569, 105.421, 0.753009, 300, 0, 0, 356, 0, 0, 0, 0, 0),
+(@ENTRY, 0, 1, 1, 0, 0, -9096.52, 839.324, 97.629, 4.02503, 300, 0, 0, 356, 0, 0, 0, 0, 0),
+(@ENTRY, 0, 1, 1, 0, 0, -9110.89, 828.068, 97.6299, 1.25651, 300, 0, 0, 356, 0, 0, 0, 0, 0);
