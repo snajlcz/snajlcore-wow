623aca48fe1b7894dbeabe5024c941ae0628fa47
Vitasic <get_email Vitasic>
2013-05-25 21:03:47 +0400

    Core/Quest: Fix: The Emissary
    http://www.wowhead.com/quest=11626/the-emissary
    Former-commit-id: 23e42f7626b206348f5415f1a363b2096e6a4423 [formerly 0648fa8a07bebb1cc19c8b0a1d261fece1c92c52]
    Former-commit-id: 8e336b2ba7642fda06d887d94ec62fa8d83268fd

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -2002,7 +2002,7 @@ class spell_item_impale_leviroth : public SpellScriptLoader
             void HandleDummy(SpellEffIndex /* effIndex */)
             {
                 if (Unit* target = GetHitCreature())
-                    if (target->GetEntry() == NPC_LEVIROTH && !target->HealthBelowPct(95))
+                    if (target->GetEntry() == NPC_LEVIROTH && target->HealthBelowPct(95)) // quest=11626
                         target->CastSpell(target, SPELL_LEVIROTH_SELF_IMPALE, true);
             }
 
