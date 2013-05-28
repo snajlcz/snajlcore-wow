b985592748249348cb6c4c76c16e89e87f09b9e9
Vitasic <get_email Vitasic>
2013-05-28 23:21:25 +0400

    DB/NPC: Fix: Elite NPCs in Northend.
    Former-commit-id: 5c23c94a07d8b49816a461257e8cdeb999295541 [formerly a94b1a16e515f788227dc544d2b9ad05953c04ed]
    Former-commit-id: abaff31db90c6887fb307ab5e8690e0829c85b04

--- sql/Battle/world.sql
+++ sql/Battle/world.sql
@@ -324,3 +324,14 @@ UPDATE `creature_template` SET `mechanic_immune_mask` = 1 WHERE `entry` IN (1942
 -- http://ru.wowhead.com/npc=9543
 -- http://ru.wowhead.com/npc=10043
 UPDATE `creature_template` SET `faction_A` = 7, `faction_H` = 7 WHERE `entry` IN (9543,10043); -- 735 > 7
+
+SET @LOQUENAHAK   := 32517; -- http://ru.wowhead.com/npc=32517/loquenahak
+SET @SKOLL        := 35189; -- http://ru.wowhead.com/npc=35189/skoll
+SET @GONDRIA      := 33776; -- http://ru.wowhead.com/npc=33776/gondria
+SET @ARCTURIS     := 38453; -- http://ru.wowhead.com/npc=38453/arcturis
+DELETE FROM `creature` WHERE `id` IN (@LOQUENAHAK, @SKOLL, @GONDRIA, @ARCTURIS);
+INSERT INTO `creature`(guid,id,map,spawnMask,phaseMask,modelid,equipment_id,position_x,position_y,position_z,orientation,spawntimesecs,spawndist,currentwaypoint,curhealth,curmana,MovementType,npcflag,unit_flags,dynamicflags) VALUES
+('200139', @LOQUENAHAK, '571', '1', '1', '0', '0', '4936.6', '4021.25', '-18.33', '4.68883', '480', '10', '0', '16502', '0', '1', '0', '0', '0'),
+('42159', @SKOLL, '571', '1', '1', '0', '0', '7178.03', '-1550.96', '931.00', '0.178631', '39600', '0', '0', '1', '0', '0', '0', '0', '0'),
+('200131', @GONDRIA, '571', '1', '1', '0', '0', '6359.04', '-3777.58', '459.06', '5.37384', '14400', '5', '0', '17068', '0', '1', '0', '0', '0'),
+('42158', @ARCTURIS, '571', '1', '1', '0', '0', '3390.91', '-2647.57', '50.715', '0', '39600', '0', '0', '1', '0', '0', '0', '0', '0');
