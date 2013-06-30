ce2ffe4be1ea2655ee2cb8672462d99d2dad2f5c
JarDv <get_email JarDv>
2013-06-29 22:04:44 -0600

    6.49 - SQL/World/Udates: added file 2013_06_29_01_100_world_IoC_creature_template.sql by Jar
    Former-commit-id: 6a651cb079fa76d2add287a2b69d8df4f3ee5764 [formerly 4b3a9f7cc1a484fc7ca6e42fc15b85b2c8f49b47]
    Former-commit-id: b9040f4b8b87f38e469ab7cd13df0d354eab76d2

---
+++ sql/updates/world/2013_06_29_01_100_world_Isle_of_Conquest_creature_template.sql
@@ -0,0 +1,26 @@
+-- NPC_HIGH_COMMANDER_HALFORD_WYRMBANE
+UPDATE creature_template SET minlevel=81 , maxlevel=81  WHERE entry=34924 LIMIT 1;
+-- NPC_OVERLORD_AGMARs
+UPDATE creature_template SET minlevel=81 , subname="<Kor'kron Commander>" , maxlevel=81  WHERE entry=34922 LIMIT 1;
+-- NPC_KOR_KRON_GUARD
+UPDATE creature_template SET minlevel=80 , maxlevel=80  WHERE entry=34918 LIMIT 1;
+-- NPC_SEVEN_TH_LEGION_INFANTRY
+UPDATE creature_template SET minlevel=80 , maxlevel=80  WHERE entry=34919 LIMIT 1;
+-- NPC_DEMOLISHER
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=34775 LIMIT 1;
+-- NPC_SIEGE_ENGINE_H
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=35069 LIMIT 1;
+-- NPC_SIEGE_ENGINE_A
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=34776 LIMIT 1;
+-- NPC_GLAIVE_THROWER_A
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=34802 LIMIT 1;
+-- NPC_GLAIVE_THROWER_H
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=35273 LIMIT 1;
+-- NPC_CATAPULT
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0  WHERE entry=34793 LIMIT 1;
+-- NPC_HORDE_GUNSHIP_CANNON
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0 , speed_walk=0 , speed_run=0 WHERE entry=34935 LIMIT 1;
+-- NPC_ALLIANCE_GUNSHIP_CANNON
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0 , speed_walk=0 , speed_run=0 WHERE entry=34929 LIMIT 1;
+-- NPC_KEEP_CANNON
+UPDATE creature_template SET minlevel=80 , maxlevel=80 , RegenHealth=0 , speed_walk=0 , speed_run=0 WHERE entry=34944 LIMIT 1;
