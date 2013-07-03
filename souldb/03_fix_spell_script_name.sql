2fcddc1c2b2920909c175bb5f2667f9bef37eafa
Snajl <get_email Snajl>
2013-07-04 01:07:36 +0200

    Core/sql: typo
    Former-commit-id: a92f56f36018044d16e6bb566557ba7089300f25

--- souldb/03_fix_spell_script_name.sql
+++ souldb/03_fix_spell_script_name.sql
@@ -51,6 +51,6 @@ DELETE FROM `spell_script_names` WHERE `spell_id` IN (51134,51105,51107);
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (51134,'spell_random_ingredient'),
 (51105,'spell_random_ingredient'),
-(51107,'spell_random_ingredient')
+(51107,'spell_random_ingredient'),
 (51046,'spell_pot_check');
 
