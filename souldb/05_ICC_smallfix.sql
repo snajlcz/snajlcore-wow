61862e4ee0b5b243265935396c91f8591c881b04
Wallker <get_email Wallker>
2013-07-04 22:57:44 +0200

    ICC_mixfix_and_spell_brazier_hit(Midsummar Fire Festival)
    Former-commit-id: 30043d820a7e1c4b1bf35bf7325d655a34180c40 [formerly 5b4b50af8e00903b48f756bfc603db3a67091ec5]
    Former-commit-id: 72c4d38be2062f646a15b2565f9c6a641da18bef

---
+++ souldb/05_ICC_smallfix.sql
@@ -0,0 +1,12 @@
+UPDATE `creature_template` SET `mechanic_immune_mask` = '650854271' WHERE `creature_template`.`entry` =36791 LIMIT 1 ;
+UPDATE `creature_template` SET `mechanic_immune_mask` = '650854271' WHERE `creature_template`.`entry` =38169 LIMIT 1 ;
+UPDATE `creature_template` SET `mechanic_immune_mask` = '650854271' WHERE `creature_template`.`entry` =38722 LIMIT 1 ;
+UPDATE `creature_template` SET `mechanic_immune_mask` = '650854271' WHERE `creature_template`.`entry` =38721 LIMIT 1 ;
+
+UPDATE `creature_template` SET `speed_walk` = '0.85',`speed_run` = '0.85' WHERE `creature_template`.`entry` =36899 LIMIT 1 ;
+
+UPDATE `creature_template` SET `speed_walk` = '0.85',`speed_run` = '0.85' WHERE `creature_template`.`entry` =38123 LIMIT 1 ;
+
+delete from `creature_template` where entry = 37744;
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES
+(37744, 0, 0, 0, 0, 0, 169, 11686, 0, 0, 'Frost Freeze Trap', '', '', 0, 80, 80, 0, 14, 14, 0, 1, 1.14286, 1, 0, 2, 2, 0, 24, 1, 0, 0, 1, 33554432, 8, 0, 0, 0, 0, 0, 1, 1, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 4, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 128, 'npc_frost_freeze_trap', 12340)
