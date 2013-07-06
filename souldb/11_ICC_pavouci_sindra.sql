cbbfe437f9fcd17a25f30b09a334383d3a8ec5e3
Wallker <get_email Wallker>
2013-07-07 01:18:56 +0200

    SQL_ICC_pavouci_event_sindra

---
+++ souldb/11_ICC_pavouci_sindra.sql
@@ -0,0 +1,6 @@
+
+-- doplnit GUID !!
+ 
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(doplnit, 37503, 631, 15, 1, 0, 0, 4182.36, 2482.91, 211.033, 3.12355, 300, 0, 0, 27890000, 8516000, 0, 0, 33554432, 0);
+
