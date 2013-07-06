17f5bc58cd97fcc2f6e149f3ad6044f44f494187
Wallker <get_email Wallker>
2013-07-06 23:08:11 +0200

    SQL_ICC_5BUFF

---
+++ souldb/10_ICC_5BUFF.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_area` WHERE `area`=4812 AND `spell` IN (73822,73828,73821,73827,73820,73826,73819,73825,73818,73824,73816,73762);
+
+INSERT INTO `spell_area` (`spell`,`area`,`racemask`,`gender`,`autocast`) VALUES
+
+(73816,4812,690,2,1),
+
+(73762,4812,1101,2,1);
