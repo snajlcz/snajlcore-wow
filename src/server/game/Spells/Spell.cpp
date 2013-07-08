95e6152951d8dc02a095d56478319b636522cfba
Wallker <get_email Wallker>
2013-07-09 00:39:52 +0200

    Compile error and crash fix Killing spree
    Former-commit-id: 20406c21820e7b2be9cc64cf8b8f1c3287bc36fc

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5103,7 +5103,7 @@ SpellCastResult Spell::CheckCast(bool strict)
                 {
                    float range = 10.0f;
                    Unit *target = NULL;
-                   Trinity::AnyUnfriendlyAttackableVisibleUnitInObjectRangeCheck u_check(m_caster, target, range);
+                   Trinity::AnyUnfriendlyAttackableVisibleUnitInObjectRangeCheck u_check(target, range);
                    Trinity::UnitLastSearcher<Trinity::AnyUnfriendlyAttackableVisibleUnitInObjectRangeCheck> checker(m_caster, target, u_check);
                    m_caster->VisitNearbyObject(range, checker);
                   
