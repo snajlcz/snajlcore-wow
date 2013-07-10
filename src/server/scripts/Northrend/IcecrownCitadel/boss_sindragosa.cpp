8aad58ce82876cb2c8a6970abac3ad5e1fd3618b
Wallker <get_email Wallker>
2013-07-10 02:48:21 +0200

    ICC_sindra land
    Former-commit-id: 667910f4b23d1485d699106b792d4ff011a270e3

--- src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
@@ -262,6 +262,7 @@ class boss_sindragosa : public CreatureScript
                 instance->SetBossState(DATA_SINDRAGOSA, FAIL);
                 me->SetCanFly(false);
                 me->SetDisableGravity(false);
+                instance->SetData(DATA_SINDRAGOSA_FROSTWYRMS,0);
                 me->GetMap()->SummonCreature(NPC_SPINESTALKER, SpinestalkerFlyPos);
                 me->GetMap()->SummonCreature(NPC_RIMEFANG, RimefangFlyPos);
                 me->DisappearAndDie();
@@ -1585,7 +1586,7 @@ class at_sindragosa_lair : public AreaTriggerScript
 
                 if (!instance->GetData(DATA_RIMEFANG))
                     if (Creature* rimefang = ObjectAccessor::GetCreature(*player, instance->GetData64(DATA_RIMEFANG)))
-                        rimefang->AI()->DoAction(ACTION_START_FROSTWYRM);
+                         rimefang->AI()->DoAction(ACTION_START_FROSTWYRM);
 
                 if (!instance->GetData(DATA_SINDRAGOSA_FROSTWYRMS) && !instance->GetData64(DATA_SINDRAGOSA) && instance->GetBossState(DATA_SINDRAGOSA) != DONE)
                 {
