924d0a64f7563c365b8ba7d8e573bb973c6a5dad
Wallker <get_email Wallker>
2013-07-14 22:08:25 +0200

    Cannibalize
    Former-commit-id: e7a3d3841941ce241cbc568c4b355df229ad61f1 [formerly ffc34eed616e43d1ea1102fde237c3f22a93c410]
    Former-commit-id: 5b446aa83d2f5ce97fa1e6af6aff40b7091e74f1

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3019,6 +3019,9 @@ void SpellMgr::LoadDbcDataCorrections()
             case 36350: //They Must Burn Bomb Aura (self)
                 spellInfo->EffectTriggerSpell[0] = 36325; // They Must Burn Bomb Drop (DND)
                 break;
+            case 20577: // Cannibalize
+               spellInfo->AttributesEx6 |= SPELL_ATTR6_CAN_TARGET_INVISIBLE;
+               break;
             case 49838: // Stop Time
             case 5171: // Slice and Dice rank1
             case 6774: // Slice and Dice rank2
