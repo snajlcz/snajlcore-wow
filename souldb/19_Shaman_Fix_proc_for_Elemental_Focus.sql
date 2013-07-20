9df76d3c194836feb00f899a3179d5f584b2fa1a
Wallker <T.Petyrek@seznam.cz>
2013-07-20 02:30:13 +0200

    DB/Procs_risen_ally_and_proc for Elemental Focus
    Former-commit-id: 89b89856234432c55e666e2e5bb43a9c22b04f39

---
+++ souldb/19_Shaman_Fix_proc_for_Elemental_Focus.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=16164;
+
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(16164, 0x1C, 0x0B, 0x98101417, 0x80043000, 0x00002004, 0x00010000, 0x00000002, 0, 100, 0);
