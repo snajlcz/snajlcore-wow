8d8e51d9c6ff8b1a3def2e628e5b8cb3d8b57ba5
Wallker <T.Petyrek@seznam.cz>
2013-08-14 02:22:52 +0200

    Core/BG typo2

--- src/server/game/Scripting/ScriptMgr.cpp
+++ src/server/game/Scripting/ScriptMgr.cpp
@@ -1252,6 +1252,11 @@ void ScriptMgr::OnPlayerBindToInstance(Player* player, Difficulty difficulty, ui
     FOREACH_SCRIPT(PlayerScript)->OnBindToInstance(player, difficulty, mapid, permanent);
 }
 
+void ScriptMgr::OnPlayerJoinedBattleground(Player* player, Battleground* bg)
+{
+    FOREACH_SCRIPT(PlayerScript)->OnPlayerJoinedBattleground(player, bg);
+}
+
 void ScriptMgr::OnPlayerFirstKillBattleground(Player* player, Battleground* bg)
 {
     FOREACH_SCRIPT(PlayerScript)->OnPlayerFirstKillBattleground(player, bg);
