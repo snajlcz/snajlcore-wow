6029aa26360ff284519eea69942e183608d3ef02
Wallker <T.Petyrek@seznam.cz>
2013-08-15 23:02:58 +0200

    Core/BG test

--- src/server/game/Cfbg/Cfbg.cpp
+++ src/server/game/Cfbg/Cfbg.cpp
@@ -48,7 +48,7 @@ bool Player::SendRealNameQuery()
 
 void Player::SetFakeRace()
 {
-     m_FakeRace = GetOTeam(); 
+     m_FakeRace = GetOTeam() == ALLIANCE ? RACE_BLOODELF : RACE_HUMAN;
 }
 
 bool Player::SendBattleGroundChat(uint32 msgtype, std::string message)
